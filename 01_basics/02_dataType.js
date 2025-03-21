1e1fba11fbaa368a33cf4e5b04d537b8636cc05f
CommandoSE4 <avinashsharma872005@gmail.com>
2025-03-21 17:22:24 +0000

    DataTypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,9 @@
+"use strict"; //treat all JS code as newer version
+              //Can use ; not necessary
+//alert(3+3) //we are using node js not browser
+             
+console.log(3 //JS ignores spaces
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
