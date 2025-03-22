a25c6765ee179fb3db86d0e6dd82ed2411ff7817
CommandoSE4 <avinashsharma872005@gmail.com>
2025-03-22 21:16:47 +0000

    dataType

--- 01_basics/02_dataType.js
+++ 01_basics/02_dataType.js
@@ -7,3 +7,25 @@ console.log(3 //JS ignores spaces
      3) // code readability should be high
 
 console.log("Hitesh")
+
+let name = "Avinash"
+let age = 20
+let isLoggedIn = true
+let state;
+let city = null
+
+/* PRIMITIVE DATATYPES */
+// number => 2^(53)
+// bigint
+// string = ""
+// boolean = true/false
+// null =>standalone value
+// undefined = undefined
+
+/* Non Primitive Datatypes */
+// object
+
+console.log(typeof "Avinash");
+console.log(typeof age)
+console.log(typeof null);      // object
+console.log(typeof undefined); // undefined
