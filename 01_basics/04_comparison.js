a80b900b51c7dc5bc5d51c266f9103dd0f6b6756
CommandoSE4 <avinashsharma872005@gmail.com>
2025-03-23 17:37:11 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);      //The reason is that an equality check == and
+console.log(null == 0);     //comparison > < >= <= work differently
+console.log(null >= 0);     //Comparion converts null to a number, treating
+                            //it as 0 ie why null>=0 true; null==0 false
+console.log(undefined == 0);
+console.log(undefined > 0);     //all false
+console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2);
