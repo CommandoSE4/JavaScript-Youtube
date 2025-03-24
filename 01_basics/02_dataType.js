3198f25fc59a1f48d5420354e3d89aee2a4d34a3
CommandoSE4 <avinashsharma872005@gmail.com>
2025-03-24 19:25:18 +0000

    dataTypes

--- 01_basics/02_dataType.js
+++ 01_basics/02_dataType.js
@@ -1,6 +1,6 @@
 "use strict"; //treat all JS code as newer version
               //Can use ; not necessary
-//alert(3+3) //we are using node js not browser
+//alert(3+3)  //we are using node js not browser
              
 console.log(3 //JS ignores spaces
     +
