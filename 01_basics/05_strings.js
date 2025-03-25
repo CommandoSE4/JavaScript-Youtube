442309ea8bb40965891a652c4136d3ee3225f854
CommandoSE4 <avinashsharma872005@gmail.com>
2025-03-25 20:01:42 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,7 +5,27 @@ const repoCount = 50
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hiteshhc')
+const gameName = new String('hitesh-hc-com')
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(name.indexOf('v'));
+
+const newString = gameName.substring(0 , 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8 , 4)
+console.log(anotherString);
+
+const newStringOne = "  hitesh  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20', '-'));
+console.log(url.includes('sundar'));
+
+console.log(gameName.split('-'));
