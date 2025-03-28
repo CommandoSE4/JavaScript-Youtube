d4c574ce22ede51d09d0ec34d73f49f4023127d9
CommandoSE4 <avinashsharma872005@gmail.com>
2025-03-28 18:27:31 +0000

    objects and array

---
+++ 02_basics/02_array.js
@@ -0,0 +1,28 @@
+const marvel_heros = ["Ironman", "thor", "spiderman"]
+const dc_heros = ["superman", "flash", "spiderman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+// all_new_heros = [...marvel_heros,...dc_heros]
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name: "hitesh"}))   //empty array
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
